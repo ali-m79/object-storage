c8afef491ab45cfceac6fc434dd3c19ca4931de6
Ali Mashayekh <93466358+ali-m79@users.noreply.github.com>
2024-07-04 10:02:31 +0330

    Update urls.py

--- ObjectStorage/storage/urls.py
+++ ObjectStorage/storage/urls.py
@@ -4,7 +4,7 @@ from django.urls import path, include
 from .views import *
 urlpatterns = [
     path('upload/',UploadObjectView.as_view(),name="upload" ),
-    path("main/", ObjectStorageView.as_view(), name="main"),
+    path("", ObjectStorageView.as_view(), name="main"),
     path('delete/<int:obj_id>/', DeleteObjectView.as_view(), name='delete'),
     path('download/<int:obj_id>/', DownloadObjectView.as_view(), name='download'),
     path('fetch-users/<int:obj_id>/', PopupView.as_view(), name='popup'),
